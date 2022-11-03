c9d05b4641f4279785a1340b63990ec415c110bf
“Akpughe” <davidakpughe2@gmail.com>
2022-11-03 23:27:21 +0100

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Davak | David Akpughe</title>
+        <meta
+          name="description"
+          content="Frontend developer and Javascript engineer. I enjoy creating frontend solutions that are reliable, scalable, and have excellent user experiences. - David Akpughe"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Davak | David Akpughe</title>
-        <meta name="description" content="Frontend developer and Javascript engineer. I enjoy creating frontend solutions that are reliable, scalable, and have excellent user experiences. - David Akpughe" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
         <link rel="canonical" href="https://www.davak.vercel.app/" />
